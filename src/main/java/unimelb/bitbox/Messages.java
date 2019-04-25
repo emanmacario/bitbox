18b4a5f654cc14f871f23e7287219c5e6b94b619
emanmacario <emacario1997@hotmail.com>
2019-04-25 21:04:54 +1000

    Made all methods static

--- src/main/java/unimelb/bitbox/Messages.java
+++ src/main/java/unimelb/bitbox/Messages.java
@@ -15,7 +15,7 @@ public class Messages {
 
 	}
 	
-	public String getInvalidProtocol(String message) {
+	public static String getInvalidProtocol(String message) {
 		Document doc1 = new Document();
 		doc1.append("command","INVALID_PROTOCOL");
 		doc1.append("messsage",message);
@@ -23,7 +23,7 @@ public class Messages {
         return doc1.toJson();
 	}
 	
-	public String getHandshakeRequest(String host, int port) {
+	public static String getHandshakeRequest(String host, int port) {
 		Document doc1 = new Document();
         doc1.append("host",host);
         doc1.append("port",port);
@@ -34,7 +34,7 @@ public class Messages {
         return doc2.toJson();
 	}
 	
-	public String getHandshakeResponse(String host, int port) {
+	public static String getHandshakeResponse(String host, int port) {
 		Document doc1 = new Document();
         doc1.append("host",host);
         doc1.append("port",port);
@@ -45,7 +45,7 @@ public class Messages {
         return doc2.toJson();
 	}
 	
-	public String getConnectionRefused(Map<String,Integer> peers, String message) {
+	public static String getConnectionRefused(Map<String,Integer> peers, String message) {
 		ArrayList<Document> peerList = new ArrayList<Document>();
 		for (Map.Entry<String, Integer> entry : peers.entrySet()) {
             String host = entry.getKey();
@@ -63,32 +63,28 @@ public class Messages {
         return doc.toJson();
 	}
 	
-	public String getFileCreateRequest(String md5, Long lastModified, Long fileSize, String pathName) {
+	public static String getFileCreateRequest(String md5, Long lastModified, Long fileSize, String pathName) {
 		Document doc1 = new Document();
         doc1.append("md5",md5);
         doc1.append("lastModified",lastModified.toString());
         doc1.append("fileSize",fileSize.toString());
         
         Document doc2 = new Document();
-        
 		doc2.append("fileDescriptor",doc1);
-        
         doc2.append("command","FILE_CREATE_REQUEST");
         doc2.append("pathName",pathName);
         
         return doc2.toJson();
 	}
 	
-	public String getFileCreateResponse(String md5, Long lastModified, Long fileSize, String pathName, String message, boolean status) {
+	public static String getFileCreateResponse(String md5, Long lastModified, Long fileSize, String pathName, String message, boolean status) {
 		Document doc1 = new Document();
         doc1.append("md5",md5);
         doc1.append("lastModified",lastModified.toString());
         doc1.append("fileSize",fileSize.toString());
         
         Document doc2 = new Document();
-        
 		doc2.append("fileDescriptor",doc1);
-        
         doc2.append("command","FILE_CREATE_RESPONSE");
         doc2.append("pathName",pathName);
         doc2.append("message", message);
@@ -96,16 +92,14 @@ public class Messages {
         return doc2.toJson();
 	}
 	
-	public String getFileBytesRequest(String md5, Long lastModified, Long fileSize, String pathName, long position, long length) {
+	public static String getFileBytesRequest(String md5, Long lastModified, Long fileSize, String pathName, long position, long length) {
 		Document doc1 = new Document();
         doc1.append("md5",md5);
         doc1.append("lastModified",lastModified.toString());
         doc1.append("fileSize",fileSize.toString());
         
         Document doc2 = new Document();
-        
 		doc2.append("fileDescriptor",doc1);
-        
         doc2.append("command","FILE_BYTES_REQUEST");
         doc2.append("pathName",pathName);
         doc2.append("position", position);
@@ -113,16 +107,14 @@ public class Messages {
         return doc2.toJson();
 	}
 	
-	public String getFileBytesResponse(String md5, Long lastModified, Long fileSize, String pathName, long position, long length, String content,String message, boolean status) {
+	public static String getFileBytesResponse(String md5, Long lastModified, Long fileSize, String pathName, long position, long length, String content,String message, boolean status) {
 		Document doc1 = new Document();
         doc1.append("md5",md5);
         doc1.append("lastModified",lastModified.toString());
         doc1.append("fileSize",fileSize.toString());
         
         Document doc2 = new Document();
-        
 		doc2.append("fileDescriptor",doc1);
-        
         doc2.append("command","FILE_BYTES_RESPONSE");
         doc2.append("pathName",pathName);
         doc2.append("position", position);
@@ -133,73 +125,67 @@ public class Messages {
         return doc2.toJson();
 	}
 	
-	public String getFileDeleteRequest(String md5, Long lastModified, Long fileSize, String pathName) {
+	public static String getFileDeleteRequest(String md5, Long lastModified, Long fileSize, String pathName) {
 		Document doc1 = new Document();
         doc1.append("md5",md5);
         doc1.append("lastModified",lastModified.toString());
         doc1.append("fileSize",fileSize.toString());
         
         Document doc2 = new Document();
-        
 		doc2.append("fileDescriptor",doc1);
-        
         doc2.append("command","FILE_DELETE_REQUEST");
         doc2.append("pathName",pathName);
         
         return doc2.toJson();
 	}
 	
-	public String getFileDeleteResponse(String md5, Long lastModified, Long fileSize, String pathName,String message, boolean status) {
+	public static String getFileDeleteResponse(String md5, Long lastModified, Long fileSize, String pathName,String message, boolean status) {
 		Document doc1 = new Document();
         doc1.append("md5",md5);
         doc1.append("lastModified",lastModified.toString());
         doc1.append("fileSize",fileSize.toString());
         
         Document doc2 = new Document();
-        
 		doc2.append("fileDescriptor",doc1);
-        
         doc2.append("command","FILE_DELETE_RESPONSE");
         doc2.append("pathName",pathName);
         doc2.append("message", message);
         doc2.append("status", status);
+
         return doc2.toJson();
 	}
 	
-	public String getFileModifyRequest(String md5, Long lastModified, Long fileSize, String pathName) {
+	public static String getFileModifyRequest(String md5, Long lastModified, Long fileSize, String pathName) {
 		Document doc1 = new Document();
         doc1.append("md5",md5);
         doc1.append("lastModified",lastModified.toString());
         doc1.append("fileSize",fileSize.toString());
         
         Document doc2 = new Document();
-        
 		doc2.append("fileDescriptor",doc1);
-        
         doc2.append("command","FILE_MODIFY_REQUEST");
         doc2.append("pathName",pathName);
         
         return doc2.toJson();
 	}
 	
-	public String getFileModifyResponse(String md5, Long lastModified, Long fileSize, String pathName,String message, boolean status) {
+	public static String getFileModifyResponse(String md5, Long lastModified, Long fileSize, String pathName,String message, boolean status) {
 		Document doc1 = new Document();
         doc1.append("md5",md5);
         doc1.append("lastModified",lastModified.toString());
         doc1.append("fileSize",fileSize.toString());
         
         Document doc2 = new Document();
-        
 		doc2.append("fileDescriptor",doc1);
-        
         doc2.append("command","FILE_MODIFY_RESPONSE");
         doc2.append("pathName",pathName);
         doc2.append("message", message);
         doc2.append("status", status);
+
         return doc2.toJson();
 	}
 	
-	public String getDirectoryCreateRequest(String pathName) {
+	public static String getDirectoryCreateRequest(String pathName) {
 		Document doc1 = new Document();
         doc1.append("command","DIRECTORY_CREATE_REQUEST");
         doc1.append("pathName",pathName);
@@ -207,7 +193,7 @@ public class Messages {
         return doc1.toJson();
 	}
 	
-	public String getDirectoryCreateResponse(String pathName, String message, boolean status) {
+	public static String getDirectoryCreateResponse(String pathName, String message, boolean status) {
 		Document doc1 = new Document();
         doc1.append("command","DIRECTORY_CREATE_RESPONSE");
         doc1.append("pathName",pathName);
@@ -217,7 +203,7 @@ public class Messages {
         return doc1.toJson();
 	}
 	
-	public String getDirectoryDeleteRequest(String pathName) {
+	public static String getDirectoryDeleteRequest(String pathName) {
 		Document doc1 = new Document();
         doc1.append("command","DIRECTORY_DELETE_REQUEST");
         doc1.append("pathName",pathName);
@@ -225,7 +211,7 @@ public class Messages {
         return doc1.toJson();
 	}
 	
-	public String getDirectoryDeleteResponse(String pathName, String message, boolean status) {
+	public static String getDirectoryDeleteResponse(String pathName, String message, boolean status) {
 		Document doc1 = new Document();
         doc1.append("command","DIRECTORY_DELETE_RESPONSE");
         doc1.append("pathName",pathName);
