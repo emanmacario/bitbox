a1217f50c34e4afe2cec5faafcccf8ad38315ab3
Michelle Keane <shellyk1997@gmail.com>
2019-04-28 15:13:31 +1000

    Most REQUEST cases given basic coding according to pdf specs, in handleJsonClientMsg() in ServerMain.java

--- src/main/java/unimelb/bitbox/ServerMain.java
+++ src/main/java/unimelb/bitbox/ServerMain.java
@@ -1,12 +1,10 @@
 package unimelb.bitbox;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
 import java.net.Socket;
 import java.net.SocketException;
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
 import java.security.NoSuchAlgorithmException;
 import java.util.HashMap;
 import java.util.LinkedList;
@@ -31,8 +29,9 @@ public class ServerMain implements FileSystemObserver, Runnable {
 	private Socket clientSocket;
 	private BufferedReader in;
 	private BufferedWriter out;
-	
-	private Queue<String> eventsQ = new LinkedList<>();
+    Messages json = new Messages();
+
+    private Queue<String> eventsQ = new LinkedList<>();
 	
 	public ServerMain() throws NumberFormatException, IOException, NoSuchAlgorithmException {
 		//Main Server (self) constructor
@@ -47,18 +46,22 @@ public class ServerMain implements FileSystemObserver, Runnable {
 		this.out = out;
 	}
 
-	@Override
-	public void processFileSystemEvent(FileSystemEvent fileSystemEvent) {
-		//System.out.println("TEST");
-		System.out.println(fileSystemEvent.event.name());
-		eventsQ.add(fileSystemEvent.event.name());
-		 System.out.println( eventsQ.toString());
-		System.out.println( eventsQ.size());
-		
+    private void directoryDeleteContents(File file, File original) {
+	    File[] contents = file.listFiles();
+	    if (contents != null) {
+            for (File f : contents) {
+                directoryDeleteContents(f, original);
+            }
+        }
+	    if (file != original) {
+	        file.delete();
+        }
 	}
 
+
     private void handleJsonClientMsg(Document json, Socket clientSocket, BufferedReader in,BufferedWriter out) throws IOException, NoSuchAlgorithmException {
-        String command = json.getString("command");
+        Long blockSize = Long.parseLong(Configuration.getConfigurationValue("blockSize"));
+	    String command = json.getString("command");
         String invalidProtocol;
         Document fileDescriptor;
         String md5;
@@ -69,6 +72,7 @@ public class ServerMain implements FileSystemObserver, Runnable {
         Long position;
         Long length;
         String content;
+        ByteBuffer src;
 
         boolean status;
 
@@ -82,29 +86,53 @@ public class ServerMain implements FileSystemObserver, Runnable {
                 fileSize = fileDescriptor.getString("fileSize");
 
                 pathName = json.getString("pathName");
+                status = false; // false until proven otherwise
+                if (Long.parseLong(fileSize)<=blockSize){
+                    length = Long.parseLong(fileSize);
+                }else{
+                    length = blockSize;
+                }
 
+                // TODO Check all this
                 // Check message credibility
-                // Check lastModified, check fileSize, and pathName (not sure what md5 is)
-                //if ()
+                if (fileSystemManager.isSafePathName(pathName)) {
+                    try {
+                        if (!fileSystemManager.fileNameExists(pathName)) {
+                            if (fileSystemManager.createFileLoader(pathName, md5, length, Long.parseLong(lastModified))) {
+                                fileSystemManager.checkShortcut(pathName);
+                                status = true;
+                                message = "file created";
+                            } else {
+                                System.out.println("Error: FileLoader creation unsuccessful");
+                                message = "there was a problem creating the file";
+                            }
+                        }else {
+                            System.out.println("Error: File " + pathName + " already exists!");
+                            message = "pathname already exists";
+                        }
+                        /*File new_file = new File(pathName);
+                        if (!fileSystemManager.fileNameExists(pathName)) {
+                            new_file.createNewFile();
+                            status = true;
+                            System.out.println(pathName + " File Created");
+                        } else {
+                            System.out.println("Error: File " + pathName + " already exists!");
+                            status = false;
+                        }*/
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        message = "there was a problem creating the file";
+                    }
+                } else {
+                    System.out.println("Error: PathName " + pathName + " is unsafe");
+                    message = "unsafe pathname given";
+                    status = false;
+                }
 
-                        String fileCreateResponse = this.json.getFileCreateResponse(md5, Long.parseLong(lastModified), Long.parseLong(fileSize), pathName, message, status);
-                        out.write(fileCreateResponse+"\n");
-                        out.flush();
-                        /*
-                        // Start peer thread to manage P2P communication in separate thread per peer
-
-                        Runnable runnable = new ServerMain(serverMain, clientSocket, in, out);
-                        Thread thread = new Thread(runnable);
-                        thread.start();
-                        log.info("P2P Connection Thread Running");
-
-                else {
-                    invalidProtocol = this.json.getInvalidProtocol("message must contain xx field");
-                    log.info("Invalid message between port: "+ this.port + " @ host: "+ this.advertisedHost +" and port: " + port +" @ host: " + host + invalidProtocol);
-                    out.write(invalidProtocol+"\n");
-                    out.flush();
-                }*/
-            break;
+                String fileCreateResponse = this.json.getFileCreateResponse(md5, Long.parseLong(lastModified), Long.parseLong(fileSize), pathName, message, status);
+                out.write(fileCreateResponse+"\n");
+                out.flush();
+                break;
 
             case "FILE_CREATE_RESPONSE":
 
@@ -117,10 +145,21 @@ public class ServerMain implements FileSystemObserver, Runnable {
                 message = json.getString("message");
                 status = json.getBoolean("status");
 
+                if (Long.parseLong(fileSize)<=blockSize){
+                    length = Long.parseLong(fileSize);
+                }else length = blockSize;
+
                 // Check whether it was successful
-                if (status == true) {
+                if (status) {
+                    // Needs to now send a FILE_BYTES_REQUEST
+                    position = Long.parseLong("0");
+                    String fileBytesRequest = this.json.getFileBytesRequest(md5, Long.parseLong(lastModified), Long.parseLong(fileSize), pathName, position, length);
+                    out.write(fileBytesRequest+"\n");
+                    out.flush();
+                    break;
                 }
                 else {
+                    // TODO File create response else statement
                     // retry?
                 }
 
@@ -134,14 +173,31 @@ public class ServerMain implements FileSystemObserver, Runnable {
 
                 pathName = json.getString("pathName");
 
-                // Check message credibility
-                //if ()
+                status = false;
 
-                    //message="depends on checks made before this point";
-                    //status; depends whether successful or not
-                    String fileDeleteResponse = this.json.getFileDeleteResponse(md5, Long.parseLong(lastModified), Long.parseLong(fileSize), pathName, message, status);
-                    out.write(fileDeleteResponse+"\n");
-                    out.flush();
+                // Check message credibility
+                if (fileSystemManager.isSafePathName(pathName)) {
+                    try {
+                        if (fileSystemManager.deleteFile(pathName, Long.parseLong(lastModified), md5)) {
+                            status = true;
+                            message = "file deleted";
+                            System.out.println(pathName + " File Deleted");
+                        } else {
+                            System.out.println("Error: File " + pathName + " does not exist");
+                            message = "file does not exist";
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        message = "there was a problem deleting the file";
+                    }
+                }else {
+                    message = "unsafe pathname given";
+                }
+
+                String fileDeleteResponse = this.json.getFileDeleteResponse(md5, Long.parseLong(lastModified), Long.parseLong(fileSize), pathName, message, status);
+                out.write(fileDeleteResponse+"\n");
+                out.flush();
+                break;
 
             case "FILE_DELETE_RESPONSE":
 
@@ -154,14 +210,15 @@ public class ServerMain implements FileSystemObserver, Runnable {
                 message = json.getString("message");
                 status = json.getBoolean("status");
 
+                // TODO File delete response
                 // Check whether it was successful
-                if (status == true) {
+                if (status) {
                 }
                 else {
                     // retry?
                 }
 
-            case "FILE_MODIFY_REQUEST":
+            case "FILE_MODIFY_REQUEST": // not done
 
                 fileDescriptor = (Document) json.get("fileDescriptor");
                 md5 = fileDescriptor.getString("md5");
@@ -170,12 +227,17 @@ public class ServerMain implements FileSystemObserver, Runnable {
 
                 pathName = json.getString("pathName");
 
+                status = false;
+                message = null;
+
+                // TODO File modify request
                 // Check message credibility
                 //if ()
 
-                    String fileModifyResponse = this.json.getFileModifyResponse(md5, Long.parseLong(lastModified), Long.parseLong(fileSize), pathName, message, status);
-                    out.write(fileModifyResponse+"\n");
-                    out.flush();
+                String fileModifyResponse = this.json.getFileModifyResponse(md5, Long.parseLong(lastModified), Long.parseLong(fileSize), pathName, message, status);
+                out.write(fileModifyResponse+"\n");
+                out.flush();
+                break;
 
             case "FILE_MODIFY_RESPONSE":
 
@@ -188,8 +250,11 @@ public class ServerMain implements FileSystemObserver, Runnable {
                 message = json.getString("message");
                 status = json.getBoolean("status");
 
+                // TODO file modify response
                 // Check whether it was successful
-                if (status == true) {
+                if (status) {
+                    // TODO
+                    message = "modify still needs to be done";
                 }
                 else {
                     // retry?
@@ -197,38 +262,85 @@ public class ServerMain implements FileSystemObserver, Runnable {
 
             case "DIRECTORY_CREATE_REQUEST":
 
-                String pathName = json.getString("pathName");
+                pathName = json.getString("pathName");
+
+                status = false;
 
                 // Check message credibility
-                //if ()
+                if (fileSystemManager.isSafePathName(pathName)) {
+                    try {
+                        if (!fileSystemManager.dirNameExists(pathName)) {
+                            status = fileSystemManager.makeDirectory(pathName);
+                            if (status) {
+                                System.out.println(pathName + " Directory Created");
+                                message = "directory created";
+                                status = true;
+                            } else {
+                                System.out.println(pathName + " Directory not Created");
+                                message = "there was a problem creating the directory";
+                            }
+                        } else {
+                            System.out.println("Error: Directory " + pathName + " already exists!");
+                            message = "pathname already exists";
+                            status = false;
+                        }
+                    } catch (Exception e) {
+                        message = "there was a problem creating the directory";
+                        e.printStackTrace();
+                    }
+                } else {
+                    System.out.println("Error: PathName " + pathName + " is unsafe");
+                    message = "unsafe pathname given";
+                    status = false;
+                }
 
-                    String directoryCreateResponse = this.json.getDirectoryCreateResponse(pathName, message, status);
-                    out.write(directoryCreateResponse+"\n");
-                    out.flush();
+                String directoryCreateResponse = this.json.getDirectoryCreateResponse(pathName, message, status);
+                out.write(directoryCreateResponse+"\n");
+                out.flush();
+                break;
 
             case "DIRECTORY_CREATE_RESPONSE":
 
                 pathName = json.getString("pathName");
                 message = json.getString("message");
-                status = json.getBoolean("status")
+                status = json.getBoolean("status");
 
+                // TODO directory create response
                 // Check whether it was successful
-                if (status == true) {
+                if (!status) {
+                    //retry?
                 }
-                else {
-                    // retry?
-                }
+
 
             case "DIRECTORY_DELETE_REQUEST":
 
                 pathName = json.getString("pathName");
-
+                status = false;
                 // Check message credibility
-                //if ()
+                if (fileSystemManager.isSafePathName(pathName)) {
+                    try {
+                        File delete_dir = new File(pathName);
+                        if (fileSystemManager.dirNameExists(pathName)) {
+                            directoryDeleteContents(delete_dir, delete_dir);
+                            status = fileSystemManager.deleteDirectory(pathName);
+                            System.out.println(pathName + " Directory Created");
+                            message = "directory created";
+                        } else {
+                            System.out.println("Error: Directory " + pathName + " does not exist!");
+                            message = "directory/pathname does not exist";
+                        }
+                    } catch (Exception e) {
+                        message = "there was a problem deleting the directory";
+                        e.printStackTrace();
+                    }
+                } else {
+                    message = "unsafe pathname given";
+                }
 
-                    String directoryDeleteResponse = this.json.getDirectoryDeleteResponse(pathName, message, status);
-                    out.write(directoryDeleteResponse+"\n");
-                    out.flush();
+                String directoryDeleteResponse = this.json.getDirectoryDeleteResponse(pathName, message, status);
+                out.write(directoryDeleteResponse+"\n");
+                out.flush();
+                break;
 
             case "DIRECTORY_DELETE_RESPONSE":
 
@@ -236,11 +348,10 @@ public class ServerMain implements FileSystemObserver, Runnable {
                 message = json.getString("message");
                 status = json.getBoolean("status");
 
+                // TODO directory delete response
                 // Check whether it was successful
-                if (status == true) {
-                }
-                else {
-                    // retry?
+                if (!status) {
+                    //retry?
                 }
 
             case "FILE_BYTES_REQUEST":
@@ -253,6 +364,28 @@ public class ServerMain implements FileSystemObserver, Runnable {
                 pathName = json.getString("pathName");
                 position = json.getLong("position");
                 length = json.getLong("length");
+
+                status = false;
+                content = null;
+
+                src = fileSystemManager.readFile(md5, position, length);
+                if (src!=null) {
+                    // convert src to content string
+                    try {
+                        content = StandardCharsets.UTF_8.decode(src).toString();
+                        message = "successfully read";
+                        status=true;
+                    } catch (Exception e) {
+                        message = "unsuccessfully read";
+                        e.printStackTrace();
+                    }
+                } else {
+                    message = "unsuccessfully read";
+                }
+                String fileBytesResponse = this.json.getFileBytesResponse(md5, Long.parseLong(lastModified), Long.parseLong(fileSize), pathName, position, length, content, message, status);
+                out.write(fileBytesResponse+"\n");
+                out.flush();
+                break;
 
             case "FILE_BYTES_RESPONSE":
 
@@ -268,8 +401,19 @@ public class ServerMain implements FileSystemObserver, Runnable {
                 content = json.getString("content");
                 status = json.getBoolean("status");
 
+                // TODO surely the below is wrong? Why do it again?
+                src = fileSystemManager.readFile(md5, position, length);
+
                 // Check whether it was successful
-                if (status == true) {
+                if (status) {
+                    fileSystemManager.writeFile(pathName, src, position);
+                    if (!fileSystemManager.checkWriteComplete(pathName)){
+                        //TODO send another file bytes request
+                        String fileBytesRequest = this.json.getFileBytesRequest(md5, Long.parseLong(lastModified), Long.parseLong(fileSize), pathName, position, length);
+                        out.write(fileBytesRequest+"\n");
+                        out.flush();
+                        break;
+                    }
                 }
                 else {
                     // retry?
@@ -283,6 +427,11 @@ public class ServerMain implements FileSystemObserver, Runnable {
                 out.write(invalidProtocol+"\n");
                 out.flush();*/
 
+                break;
+            default:
+                invalidProtocol = this.json.getInvalidProtocol("Unexpected command");
+                out.write(invalidProtocol+"\n");
+                out.flush();
         }
 
     }
@@ -360,4 +509,15 @@ public class ServerMain implements FileSystemObserver, Runnable {
 		}
 
 	}*/
+}
+// TODO
+    @Override
+    public FileSystemEvent processFileSystemEvent(FileSystemEvent fileSystemEvent) {
+        //System.out.println("TEST");
+        System.out.println(fileSystemEvent.event.name());
+        eventsQ.add(fileSystemEvent.event.name());
+        System.out.println( eventsQ.toString());
+        System.out.println( eventsQ.size());
+        return null;
+    }
 }
